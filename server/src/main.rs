bebff9e97724faf9d38e20847628e98e78798571
Junyong Tan <jytan1701@gmail.com>
2026-05-23 12:54:18 -0500

    health and ready endpoitns set up

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, extract::State, http::StatusCode, routing::get};
+use redis::aio::ConnectionManager;
+use sqlx::PgPool;
+use std::env;
+
+#[derive(Clone)]
+struct AppState {
+    db: PgPool,
+    redis: ConnectionManager,
+}
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let redis_url = env::var("REDIS_URL").expect("REDIS_URL must be set");
+
+    let db = PgPool::connect(&db_url)
+        .await
+        .expect("failed to connect to postgres");
+    println!("Connected to postgres");
+
+    let redis_client = redis::Client::open(redis_url).expect("invalid REDIS_URL");
+    let redis = ConnectionManager::new(redis_client)
+        .await
+        .expect("failed to connect to resis");
+    println!("Connected to redis");
+
+    let state = AppState { db, redis };
+
+    let app = Router::new()
+        .route("/health", get(health))
+        .route("/ready", get(ready))
+        .with_state(state);
+
+    let addr = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("listening on {addr}");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health() -> &'static str {
+    "ok"
+}
+
+async fn ready(State(mut state): State<AppState>) -> Result<&'static str, StatusCode> {
+    sqlx::query("SELECT 1")
+        .execute(&state.db)
+        .await
+        .map_err(|_| StatusCode::SERVICE_UNAVAILABLE)?;
+    redis::cmd("PING")
+        .query_async::<String>(&mut state.redis)
+        .await
+        .map_err(|_| StatusCode::SERVICE_UNAVAILABLE)?;
+    Ok("ready")
 }
