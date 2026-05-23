b00d25434f37372a4cccfcc866f46f7a4569c94d
Junyong Tan <jytan1701@gmail.com>
2026-05-23 13:45:56 -0500

    added migration to create user able

---
+++ migrations/20260523180146_create_users.up.sql
@@ -0,0 +1,11 @@
+-- Add up migration script here
+CREATE EXTENSION IF NOT EXISTS citext;
+
+CREATE TABLE users (
+	id                  UUID PRIMARY KEY,
+	username            CITEXT NOT NULL UNIQUE,
+	password_hash       TEXT NOT NULL,
+	created_at          TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE INDEX idx_users_created_at ON users (created_at DESC);
