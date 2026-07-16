e8ff66b31fab0f8442818785959156faac196c06
Junyong Tan <jytan1701@gmail.com>
2026-07-15 22:55:37 -0500

    stop tracking target/

--- target/debug/deps/shared-0f1c82ce883609b1.d
+++
@@ -1,9 +0,0 @@
-/Users/junyongtan/projects/system-design/mlsd-chatapp/target/debug/deps/shared-0f1c82ce883609b1.d: shared/src/lib.rs Cargo.toml
-
-/Users/junyongtan/projects/system-design/mlsd-chatapp/target/debug/deps/libshared-0f1c82ce883609b1.rmeta: shared/src/lib.rs Cargo.toml
-
-shared/src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- target/debug/deps/shared-b7c9f6d8a5be9e3c.d
+++
@@ -1,9 +0,0 @@
-/Users/junyongtan/projects/system-design/mlsd-chatapp/target/debug/deps/shared-b7c9f6d8a5be9e3c.d: shared/src/lib.rs Cargo.toml
-
-/Users/junyongtan/projects/system-design/mlsd-chatapp/target/debug/deps/libshared-b7c9f6d8a5be9e3c.rmeta: shared/src/lib.rs Cargo.toml
-
-shared/src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
